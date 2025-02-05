0ed59599d3a16fb01926efec3d47b3afc19cfdbc
Fernando Godoy Gonzalez <fernando.godoy.gonzalez@gmail.com>
2025-02-05 20:20:02 -0300

    Se crea funcion agregarAmigo para validar y agregar elemento ingresado por usuario a una lista

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let listaAmigos = [];
+
+function agregarAmigo() {
+    const nombreIngresado = document.getElementById("amigo");
+    const nombreAmigo = nombreIngresado.value.trim();
+
+    if (nombreAmigo == "") {
+        alert("Ingresa un nombre antes de hacer click en agregar.");
+        return;
+    }else{
+        listaAmigos.push(nombreAmigo);
+        actualizarLista();
+        input.value = "";
+    }
+}
